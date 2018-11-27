a627e9d10ecd66e5331ea3e3996ab18c5b0b35fe
David Petticrew <David.Petticrew@riversidecollege.ac.uk>
2018-11-27 10:16:59 +0000

    repeat with while

--- Move Picture/Form1.cs
+++ Move Picture/Form1.cs
@@ -21,8 +21,22 @@ namespace Move_Picture
         {
             for (int i = 0; i < 100; i++)
             {
-                pictureBox1.Location = new Point(pictureBox1.Location.X + 3, pictureBox1.Location.Y);
-                await Task.Delay(50);
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 1, pictureBox1.Location.Y);
+                await Task.Delay(1);
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X - 1, pictureBox1.Location.Y);
+                await Task.Delay(1);
+            }
+        }
+
+        private async  void button5_Click(object sender, EventArgs e)
+        {
+            while (pictureBox1.Location.X + 5 < ClientRectangle.Width - pictureBox1.Width)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+                await Task.Delay(1);
             }
         }
     }
