faadc852bc26ba5ffed68e5657d745332d08da83
David Petticrew <David.Petticrew@riversidecollege.ac.uk>
2018-11-27 10:45:37 +0000

    round the screen and centre

--- Move Picture/Form1.cs
+++ Move Picture/Form1.cs
@@ -39,5 +39,84 @@ namespace Move_Picture
                 await Task.Delay(1);
             }
         }
+
+        private async void button6_Click(object sender, EventArgs e)
+        {
+            int y=pictureBox1.Location.Y;
+            int x = pictureBox1.Location.X;
+            while (pictureBox1.Location.X + 5 < ClientRectangle.Width - pictureBox1.Width)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+                await Task.Delay(1);
+            }
+            while (pictureBox1.Location.Y - 5> 0)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y-5);
+                await Task.Delay(1);
+            }
+            while (pictureBox1.Location.X - 5 > 0)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y);
+                await Task.Delay(1);
+            }
+            while (pictureBox1.Location.Y + 5 < ClientRectangle.Height - pictureBox1.Height)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 5);
+                await Task.Delay(1);
+            }
+            while (pictureBox1.Location.X + 5 < ClientRectangle.Width - pictureBox1.Width)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+                await Task.Delay(1);
+            }
+            while (pictureBox1.Location.Y - 5 > y)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 5);
+                await Task.Delay(1);
+            }
+            while (pictureBox1.Location.X - 5 > x)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y);
+                await Task.Delay(1);
+            }
+        }
+
+        private async void button7_Click(object sender, EventArgs e)
+        {
+            int x = ClientRectangle.Width / 2 - pictureBox1.Width / 2;
+            int y = ClientRectangle.Height / 2 - pictureBox1.Height / 2;
+            if (x> pictureBox1.Location.X)
+            {
+                while (pictureBox1.Location.X + 5 < x)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X+5, pictureBox1.Location.Y);
+                    await Task.Delay(1);
+                }
+            }
+            else
+            {
+                while (pictureBox1.Location.X - 5 > x)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y);
+                    await Task.Delay(1);
+                }
+            }
+            if (y > pictureBox1.Location.Y)
+            {
+                while (pictureBox1.Location.Y + 5 < y)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y+5);
+                    await Task.Delay(1);
+                }
+            }
+            else
+            {
+                while (pictureBox1.Location.Y - 5 > y)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y-5);
+                    await Task.Delay(1);
+                }
+            }
+        }
     }
 }
